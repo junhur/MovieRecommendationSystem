b0fb4d23399833e72ba2b953cdbdbcd4d02f7c6d
Jun Hur <junhur@andrew.cmu.edu>
2022-03-29 05:11:19 -0400

    feat: Expose evaluate endpoint on port 8082

--- backend-services/favor8/src/main/java/com/example/favor8/evaluation/OnlineEvaluationController.java
+++ backend-services/favor8/src/main/java/com/example/favor8/evaluation/OnlineEvaluationController.java
@@ -1,2 +1,25 @@
-package com.example.favor8.evaluation;public class OnlineEvaluationController {
+package com.example.favor8.evaluation;
+
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@Slf4j
+@Tag(name = "EVALUATION", description = "online evaluation")
+@RequestMapping("evaluate")
+public class OnlineEvaluationController {
+
+    private final OnlineEvaluationService evaluationService;
+
+    @GetMapping
+    public String evaluate() {
+        String res = evaluationService.evaluate();
+//        log.info("req from user {}, res: {}", userId, res);
+        return res;
+    }
 }
