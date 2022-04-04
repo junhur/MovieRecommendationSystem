e484da0c122b6fddd1e863d7471b86a5a6927b36
xiangyuy <xiangyuy@andrew.cmu.edu>
2022-04-03 22:07:12 -0400

    Log training pipeline metrics with JSON file

--- machine-learning/model_training/azure_pipeline.py
+++ machine-learning/model_training/azure_pipeline.py
@@ -1,6 +1,7 @@
 from azureml.core.authentication import ServicePrincipalAuthentication
 from azureml.core import Workspace, Experiment
 from azureml.pipeline.core import PublishedPipeline
+import json
 
 if __name__=='__main__':
     sp = ServicePrincipalAuthentication(tenant_id="f70874a7-6e94-4cc9-9a3a-61dbe23c9955",  # tenantID
@@ -12,7 +13,11 @@ if __name__=='__main__':
                        subscription_id="f4126c71-f302-4cde-9386-b42a907f292d",
                        resource_group="favor8")
 
-    ppl = PublishedPipeline.get(ws, id='b92d76ae-f183-4e6f-89cb-1fa5ea778e9b')
+    ppl = PublishedPipeline.get(ws, id='0ab4967e-5fd2-42d4-a76a-7cfb9a8707d3')
 
     pipeline_run = Experiment(ws, 'recommender_train').submit(ppl)
     pipeline_run.wait_for_completion(show_output=True)
+
+    metrics = pipeline_run.get_metrics()
+    with open('metrics.json', 'a') as fp:
+        json.dump(metrics, fp)
