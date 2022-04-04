b6edfaad558fc7076618af8b944847143f3f8552
xiangyuy <xiangyuy@andrew.cmu.edu>
2022-04-03 23:09:41 -0400

    Hide authentication information

--- machine-learning/model_training/azure_compute.py
+++ machine-learning/model_training/azure_compute.py
@@ -3,14 +3,21 @@ from azureml.core.compute import ComputeInstance
 from azureml.core.compute_target import ComputeTargetException
 from azureml.core.authentication import ServicePrincipalAuthentication
 
+with open("secrets.txt") as f:
+    lines = f.readlines()
+    TENANT_ID = lines[0].strip()
+    CLIENT_ID = lines[1].strip()
+    CLIENT_SECRET = lines[2].strip()
+    SUBSCRIPTION_ID = lines[3].strip()
+
 if __name__=='__main__':
-    sp = ServicePrincipalAuthentication(tenant_id="f70874a7-6e94-4cc9-9a3a-61dbe23c9955",  # tenantID
-                                        service_principal_id="fa700399-a952-4f65-85fe-d2b3ff88007e",  # clientId
-                                        service_principal_password="kZipvNWoqj7TLVocijFDFdS~URZ1gOEU70")  # clientSecret
+    sp = ServicePrincipalAuthentication(tenant_id=TENANT_ID,  # tenantID
+                                        service_principal_id=CLIENT_ID,  # clientId
+                                        service_principal_password=CLIENT_SECRET)  # clientSecret
 
     ws = Workspace.get(name="recommender",
                        auth=sp,
-                       subscription_id="f4126c71-f302-4cde-9386-b42a907f292d",
+                       subscription_id=SUBSCRIPTION_ID,
                        resource_group="favor8")
 
     # Choose a name for your instance
